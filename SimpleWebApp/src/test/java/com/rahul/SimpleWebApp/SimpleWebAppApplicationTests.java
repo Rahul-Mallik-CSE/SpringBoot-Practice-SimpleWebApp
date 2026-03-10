05fe6344b13c1b57cd33e6c39d5b78dcb7e93067
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-10 11:31:56 +0600

    first commit

---
+++ SimpleWebApp/src/test/java/com/rahul/SimpleWebApp/SimpleWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.SimpleWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
