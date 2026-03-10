c5d01d56317b476e34d82678acadabd635cc0e79
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-03-10 11:50:51 +0600

    Add LoginController with login endpoint

---
+++ SimpleWebApp/src/main/java/com/rahul/SimpleWebApp/LoginController.java
@@ -0,0 +1,13 @@
+package com.rahul.SimpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(){
+        return "This is the login page......";
+    }
+}
