129fb15868202d26cad81999926d19034aa8b43f
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-03-11 23:51:27 +0600

    ProductController to use specific mapping annotations and update ProductService to handle missing products gracefully

--- SimpleWebApp/src/main/java/com/rahul/SimpleWebApp/controller/ProductController.java
+++ SimpleWebApp/src/main/java/com/rahul/SimpleWebApp/controller/ProductController.java
@@ -3,11 +3,14 @@ package com.rahul.SimpleWebApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rahul.SimpleWebApp.model.Product;
 import com.rahul.SimpleWebApp.service.ProductService;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
@@ -17,13 +20,18 @@ public class ProductController {
     ProductService service;
 
    
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
     }
 
-    @RequestMapping("/products/{id}")
-    public Product getProductById(int prodId){
+    @GetMapping("/products/{prodId }")
+    public Product getProductById(@PathVariable int prodId){
         return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public void addProduct( Product prod){
+        service.addProduct(prod);
     }
 }
