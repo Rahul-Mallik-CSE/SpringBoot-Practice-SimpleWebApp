227c8d9302fd4f4126faa04f2410360f871944ca
Rahul Mallik <rmallik191242@bscse.uiu.ac.bd>
2026-03-14 23:46:19 +0600

    Implement update and delete product functionality in ProductService and ProductController

--- SimpleWebApp/src/main/java/com/rahul/SimpleWebApp/controller/ProductController.java
+++ SimpleWebApp/src/main/java/com/rahul/SimpleWebApp/controller/ProductController.java
@@ -3,11 +3,12 @@ package com.rahul.SimpleWebApp.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rahul.SimpleWebApp.model.Product;
@@ -36,4 +37,17 @@ public class ProductController {
         System.out.println("Adding Product: " + prod);
         service.addProduct(prod);
     }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        System.out.println("Updating Product: " + prod);
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        System.out.println("Deleting Product with ID: " + prodId);
+        service.deleteProduct(prodId);
+    }
+    
 }
